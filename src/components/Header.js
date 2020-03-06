6a1550480a5c025708e3c41e12d3609d40ac8c7f
visua77 <nicznet@hotmail.com>
2020-03-06 13:51:09 +0100

    fixed icons in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ export const Header = () => {
   return (
     <div className="header-wrapper">
       <header><h1>Sóller Travel Blog</h1>
+        <div className="soc-media"><i class="fab fa-facebook-square"></i> <i class="fab fa-instagram-square"></i> <i class="fab fa-pinterest-square"></i></div>
       </header>
     </div>
   )
