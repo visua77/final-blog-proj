fa678399d0c7f6a2902886f35d34b657a67d0eaf
visua77 <nicznet@hotmail.com>
2020-03-06 19:04:31 +0100

    styling and added a navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { BlogPost } from './components/BlogPost'
 import { Header } from './components/Header'
+import { Nav } from './components/Nav'
 import { Footer } from './components/Footer'
 import './css/styles.css'
 
@@ -8,6 +9,7 @@ export const App = () => {
   return (
     <div>
       <Header />
+      <Nav />
       <BlogPost />
       <Footer />
     </div>
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+
+export const Nav = () => {
+  return (
+    <div className="main-nav">
+      <span class="nav-items">About</span>
+      <span class="nav-items">Places</span>
+      <span class="nav-items">Restaurants</span>  
+      <span class="nav-items">Contact</span>   
+    </div>
+  )
+}
+
