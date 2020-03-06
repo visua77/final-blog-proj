dab3de9505a538e249299adac341a282267ec5d1
visua77 <nicznet@hotmail.com>
2020-03-06 12:42:59 +0100

    working on the footer somewhat

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
 export const Footer = () => {
+
   return (
     <div className="footer-wrapper">
-
+          <footer>Copyright: {(new Date().getFullYear())} <i class="fab fa-facebook-square"></i> <i class="fab fa-instagram-square"></i> <i class="fab fa-pinterest-square"></i></footer>
     </div>
   )
 }
