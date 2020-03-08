79edb32b9d14ff2ec14afcf0d0d37334f63c75c0
visua77 <nicznet@hotmail.com>
2020-03-08 17:20:18 +0100

    working on more content, aboutpage and so on

--- src/components/BlogPosts.js
+++ src/components/BlogPosts.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
+import PlaceholderImg from '../img/73495182_10156774359523473_6779904046250590208_o.jpg' // relative path to image
 
 const URL = 'https://happy-happty.herokuapp.com/'
 
@@ -18,17 +19,20 @@ export const BlogPosts = () => {
   return (
     <div>
       {/* <h3>Latest posts:</h3> */}
-      <article>
+      <div>
         {posts.map((onepost) => (
-          <div className="card" key={onepost._id}>
-            <h2>{onepost.message}</h2>
-            <p>Fetching data from the happythoughts API just to check that the connection works</p>
-            <h3>Hearts: {onepost.hearts}</h3>
-            <p><Link key={onepost._id} to={`/blog/${onepost._id}`}>Testlink</Link></p>
-            <p>{moment(onepost.createdAt).fromNow()}</p>
+          <div className="wrapper-card" key={onepost._id}>
+            <div>
+              <h2>{onepost.message}</h2>
+              <p>Fetching data from the happythoughts API just to check that the connection works</p>
+              <h3>Hearts: {onepost.hearts}</h3>
+              <p><Link key={onepost._id} to={`/blog/${onepost._id}`}>Testlink</Link></p>
+              <p>{moment(onepost.createdAt).fromNow()}</p>
+            </div>
+            <div><img src={PlaceholderImg} className="img-posts" /></div>
           </div>
         ))}
-      </article>
+      </div>
     </div>
 
   )
