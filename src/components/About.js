08337007c9f6a42f68351bacd682fe811bedef68
visua77 <nicznet@hotmail.com>
2020-03-08 16:25:55 +0100

    making the linter happy

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Image from '../img/73495182_10156774359523473_6779904046250590208_o.jpg' // relative path to image 
+import Image from '../img/73495182_10156774359523473_6779904046250590208_o.jpg' // relative path to image
 
 export const About = () => {
   return (
     <div className="wrapper-card">
-      <div><img src={Image} className="img-about" /></div>
-          <div className="about-txt"><h3>About this blog</h3> <p>This is a comprehensive blog about a magical place called the Sóller valley, situated nort of Palma de Mallorca just 20 minutes by car on the other side of the Serra de Tramuntana. The weather is lovely, the med is nice and warm until october and the people are friendly!</p></div>
+      <div><img src={Image} className="img-about" alt="about img" /></div>
+      <div className="about-txt"><h3>About this blog</h3> <p>This is a comprehensive blog about a magical place called the Sóller valley, situated nort of Palma de Mallorca just 20 minutes by car on the other side of the Serra de Tramuntana. The weather is lovely, the med is nice and warm until october and the people are friendly!</p></div>
     </div>
   )
 }
